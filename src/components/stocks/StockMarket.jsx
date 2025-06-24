1cbae4b77d808ea32a30b34722e2907e045afb6d
Guillermo Stanke <gstanke@uc.cl>
2025-06-24 16:50:07 -0400

    fixx

--- src/components/stocks/StockMarket.jsx
+++ src/components/stocks/StockMarket.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { getAllStocks, initiatePayment} from "../../utils/api";
 import { useNavigate } from "react-router-dom";
